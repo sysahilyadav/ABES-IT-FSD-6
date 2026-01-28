8781beabbf8f053a30637c35598b92b3e3c84976
SAHIL YADAV <sahilyadav5571@gmail.com>
2026-01-28 14:59:21 +0530

    promise revision

---
+++ day_01/promises/index.js
@@ -0,0 +1,15 @@
+const myPromise=new Promise((resolve,reject)=>{
+    let marks=80;
+    if (marks>33){
+        resolve("pass");
+    }
+    else{
+        reject("fail");
+
+    }
+
+})
+myPromise.then((msg)=>console.log(msg))
+.catch((error)=>console.log("some error occured"));
+// console.log(myPromise);
+
