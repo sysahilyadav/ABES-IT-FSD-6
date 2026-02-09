475cc0aca66b70a574a43dd76affa3cf0e3f2396
SAHIL YADAV <sahilyadav5571@gmail.com>
2026-02-09 11:57:04 +0530

    error handling

--- day_01/promises/index.js
+++ day_01/promises/index.js
@@ -1,7 +1,9 @@
 const getData=async()=>{
-    const response=await fetch("https://fakestoreapi.com/products");
+   try{ const response=await fetch("https://fakestoreapi.com/products");
     const res=await response.json();
     console.log(res);
+   }
+   catch(error ){ console.log("unable to fetch data ");}
 
 }
 getData();
